f365fe7629da2c6751de11fb01ffed0810e0bb0d
Omasyo <cesare3000@outlook.com>
2023-06-22 02:50:18 +0100

    added tv and movie datasource , added data module

---
+++ core/network/src/main/java/com/quitr/snac/core/network/MovieNetworkDataSource.kt
@@ -0,0 +1,64 @@
+package com.quitr.snac.core.network
+
+import com.quitr.snac.core.network.movielist.MovieListApiModel
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.http.parameters
+
+fun getMovieNetworkDataSource(client: HttpClient): MovieNetworkDataSource =
+    DefaultMovieNetworkDataSource(client)
+
+interface MovieNetworkDataSource {
+//    suspend fun getDetails(id: Int, language: String)
+
+    suspend fun getNowPlaying(page: Int, language: String, region: String): MovieListApiModel
+
+    suspend fun getPopular(page: Int, language: String, region: String): MovieListApiModel
+
+    suspend fun getTopRated(page: Int, language: String, region: String): MovieListApiModel
+
+    suspend fun getUpcoming(page: Int, language: String, region: String): MovieListApiModel
+}
+
+private class DefaultMovieNetworkDataSource(private val client: HttpClient) :
+    MovieNetworkDataSource {
+    override suspend fun getNowPlaying(
+        page: Int,
+        language: String,
+        region: String
+    ): MovieListApiModel = getMovieList("now_playing", page, language, region)
+
+
+    override suspend fun getPopular(
+        page: Int,
+        language: String,
+        region: String
+    ): MovieListApiModel = getMovieList("popular", page, language, region)
+
+    override suspend fun getTopRated(
+        page: Int,
+        language: String,
+        region: String
+    ): MovieListApiModel = getMovieList("top_rated", page, language, region)
+
+    override suspend fun getUpcoming(
+        page: Int,
+        language: String,
+        region: String
+    ): MovieListApiModel = getMovieList("upcoming", page, language, region)
+
+    private suspend inline fun getMovieList(
+        path: String,
+        page: Int,
+        language: String,
+        region: String
+    ): MovieListApiModel =
+        client.get("/3/movie/$path") {
+            parameters {
+                append("page", page.toString())
+                append("language", language)
+                append("region", region)
+            }
+        }.body()
+}
---
+++ core/network/src/main/java/com/quitr/snac/core/network/TvNetworkDataSource.kt
@@ -0,0 +1,46 @@
+package com.quitr.snac.core.network
+
+import com.quitr.snac.core.network.movielist.MovieListApiModel
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.http.parameters
+import java.util.TimeZone
+
+interface TvNetworkDataSource {
+    suspend fun getAiringToday(page: Int, language: String, timezone: String): TvListApiModel
+
+    suspend fun getOnTheAir(page: Int, language: String, timezone: String): TvListApiModel
+
+    suspend fun getPopular(page: Int, language: String, timezone: String): TvListApiModel
+
+    suspend fun getTopRated(page: Int, language: String, timezone: String): TvListApiModel
+}
+
+class DefaultTvNetworkDataSource(private val client: HttpClient) : TvNetworkDataSource {
+    override suspend fun getAiringToday(page: Int, language: String, timezone: String) =
+        getTvList("airing_today", page, language, timezone)
+
+    override suspend fun getOnTheAir(page: Int, language: String, timezone: String) =
+        getTvList("on_the_air", page, language, timezone)
+
+    override suspend fun getPopular(page: Int, language: String, timezone: String)=
+        getTvList("popular", page, language, timezone)
+
+    override suspend fun getTopRated(page: Int, language: String, timezone: String) =
+        getTvList("top_rated", page, language, timezone)
+
+    private suspend inline fun getTvList(
+        path: String,
+        page: Int,
+        language: String,
+        timezone: String
+    ): TvListApiModel =
+        client.get("/3/tv/$path") {
+            parameters {
+                append("page", page.toString())
+                append("language", language)
+                append("timezone", timezone)
+            }
+        }.body()
+}
